ddacf30df31b3f65e9bcce6f12917d46826c0adc
David Flores <davidflores7_8@hotmail.com>
2023-12-22 09:15:04 +0000

    initial commit

---
+++ repo_info.py
@@ -0,0 +1,20 @@
+import httpx
+from prefect import flow
+
+
+@flow(log_prints=True)
+def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
+    url = f"https://api.github.com/repos/{repo_name}"
+    response = httpx.get(url)
+    response.raise_for_status()
+    repo = response.json()
+    print(f"{repo_name} repository statistics 🤓:")
+    print(f"Stars 🌠 : {repo['stargazers_count']}")
+    print(f"Forks 🍴 : {repo['forks_count']}")
+
+
+if __name__ == "__main__":
+    get_repo_info.from_source(
+        "https://github.com/davidban77/prefect-poc.git",
+        entrypoint="flows/repo_info.py:get_repo_info",
+    ).deploy(name="repo-info", work_pool_name="netpanda-k3s", build=False, push=False)
